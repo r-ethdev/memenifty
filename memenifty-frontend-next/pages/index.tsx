d8c919de394691f30a78c4f3a3327a9296903e05
forgithubs <forgithubs@protonmail.com>
2021-05-07 11:02:25 -0400

    Removed react frontend to replace with next frontend

---
+++ memenifty-frontend-next/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Memenifty" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div>Memenifty is working</div>
+      </main>
+
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  )
+}
